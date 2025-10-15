ecc482b5b6c739e4df97332e7eaa0223e8047d1d
José Yovany Beltrán Matallana <101234042+JYBM78@users.noreply.github.com>
2025-10-15 14:44:29 -0500

    feat: Conecto Frontend y Backend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Api } from './api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+  protected readonly usuarios = signal<any[]>([]);
+  private readonly api = inject(Api);
+
+  ngOnInit(): void {
+    this.api.getUsuarios().subscribe(data => {
+      this.usuarios.set(data);
+    });
+  }
 }
